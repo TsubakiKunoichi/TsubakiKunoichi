6388bbea3988ca8de70b33b511c5c4a4a73c6b54
Treibsandholz <58191427+Treibsandholz@users.noreply.github.com>
2023-03-30 22:37:16 +0200

    Tutorial finished in test.py und Bug bei alleSigned

--- WeissSchwarz.py
+++ WeissSchwarz.py
@@ -2,49 +2,54 @@ import random
 import numpy as np
 
 spc = 3 # signed pro case
-dpc = 16 # displays pro case
+dpc = 18 # displays pro case
 bpd = 12 # booster pro display
 
-xdpc = 5 # ziehe x displays aus einem case
+#xdpc = 5 # ziehe x displays aus einem case
+def anz_displays(xdpc):
+    numTests = 10000 # Anzahl an Tests
 
-numTests = 10000 # Anzahl an Tests
+    signed = list()
+    # Teste für x Cases
+    for j in range(0, numTests):
+        # Anzahl an Signed für aktuellen Pull auf 0 setzen
+        signed.append(0)
 
-signed = list()
-# Teste für x Cases
-for j in range(0, numTests):
-    # Anzahl an Signed für aktuellen Pull auf 0 setzen
-    signed.append(0)
+        # Neues Case mit x Displays generieren
+        Case = []
+        # Displays ohne Signed hinzufügen
+        # 0 = keine Signed
+        for i in range(dpc - spc):
+            Case.append(0)
+        # Displays mit Signed hinzufügen
+        # 1 = Signed
+        for i in range(spc):
+            Case.append(1)
+        # Displays im Case mischen
+        random.shuffle(Case)
 
-    # Neues Case mit x Displays generieren
-    Case = []
-    # Displays ohne Signed hinzufügen
-    # 0 = keine Signed
-    for i in range(dpc - spc):
-        Case.append(0)
-    # Displays mit Signed hinzufügen
-    # 1 = Signed
-    for i in range(spc):
-        Case.append(1)
-    # Displays im Case mischen
-    random.shuffle(Case)
+        # Anzahl x aus einem Case ziehen
+        for i in range(xdpc):
+            signed[j] += Case.pop()
 
-    # Anzahl x aus einem Case ziehen
-    for i in range(xdpc):
-        signed[j] += Case.pop()
+    # ~ gezogene Signed pro Case berechnen
+    pspc = sum(signed) / numTests # pulled signed pro case
+    keineSigned = list(filter(lambda x: x == 0, signed))
+    eineSigned = list(filter(lambda x: x == 1, signed))
+    zweiSigned = list(filter(lambda x: x == 2, signed))
+    dreiSigned = list(filter(lambda x: x == 3, signed))
+    mehrAlsEine  = list(filter(lambda x: x >= 1, signed))
 
-# ~ gezogene Signed pro Case berechnen
-pspc = sum(signed) / numTests # pulled signed pro case
-keineSigned = list(filter(lambda x: x == 0, signed))
-eineSigned = list(filter(lambda x: x == 1, signed))
-zweiSigned = list(filter(lambda x: x == 2, signed))
-dreiSigned = list(filter(lambda x: x == 3, signed))
-mehrAlsEine  = list(filter(lambda x: x >= 1, signed))
+    print("Durchschnittliche Signed Pro Case :", pspc)
 
-print("Durchschnittliche Signed Pro Case :", pspc)
+    print("Keine Signed pro Case :", len(keineSigned) / numTests)
+    print("Eine Signed pro Case :", len(eineSigned) / numTests)
+    print("Zwei Signed pro Case :", len(zweiSigned) / numTests)
+    print("Drei Signed pro Case :", len(dreiSigned) / numTests)
+    alleSigned = keineSigned.extend(eineSigned) #.extend(zweiSigned).extend(dreiSigned)
+    print("Test. Summe von 'Keine, Eine, Zwei, Drei' muss eins sein: ", len(alleSigned) / numTests);
 
-print("Keine Signed pro Case :", len(keineSigned) / numTests)
-print("Eine Signed pro Case :", len(eineSigned) / numTests)
-print("Zwei Signed pro Case :", len(zweiSigned) / numTests)
-print("Drei Signed pro Case :", len(dreiSigned) / numTests)
+    print("Mehr als eine Signed pro Case :", len(mehrAlsEine) / numTests)
 
-print("Mehr als eine Signed pro Case :", len(mehrAlsEine) / numTests)
+#for i in range(10):
+anz_displays(5);
--- test/test.py
+++ test/test.py
@@ -16,6 +16,9 @@ print ('A semicolon is not neccessary but more readable.')
 # https://python.land/introduction-to-python/the-repl
 # Python.exe can be excecuted via command promt if the path to the .exe is specified
 #   in my case: C:\Kurisu_Programme\Python>python
+#   install packages like numpy
+#   ín my case: C:\Kurisu_Programme\Python\Scripts>pip install numpy
+#A session may be ende with "exit()". After that close shell with "exit"
 
 '''Interesting observation. different floatings after excecuting simple operation via command prompt 
 4**2.3
@@ -25,8 +28,56 @@ print ('A semicolon is not neccessary but more readable.')
 '''
 
 #-------------------------------------------------------------------------------------------------
+#Funktionengrundlagen
+# https://python.land/introduction-to-python/variable
+
+result = 3*4+12
+print('result = ', result);
+result = int (5/2);
+print('result = ', result)
+
+# Force typing
+# currently doesnt do anything. If forced by compiling a precompiler of specific IDE's are needed
+# https://medium.com/@ageitgey/learn-how-to-use-static-type-checking-in-python-3-6-in-10-minutes-12c86d72677b
+from typing import Dict
+result: int
+result = 4**3.1
+print('result = ', result, 'with type ', type(result));
+print('result = Booleans, with type ', type(True), "and comparison operators ==, !=, >, < \n especially True==1 and False==0, " 
+      + "Test: ", True == 4);
+print('logical operators are "not" "and" "or"')
+
+
 #-------------------------------------------------------------------------------------------------
+# https://python.land/introduction-to-python/python-for-loop
+i=0; w = "";
+for j in "World":
+    i=i+1;
+    w = w+j+j
+print("w = ", w);
+print( f"Why should I use f to prin variables like {i} and {i+1}?");
+
 #-------------------------------------------------------------------------------------------------
+# https://python.land/introduction-to-python/functions
+def fib(a,b):
+    print(a+b)
+    return a+b
+print("Fibonacchi sequence: Values till 1000")
+a=0; b=1;
+while a < 1000:
+    a=fib(a,b)
+    b=fib(b,a)
+
+print("My favorite for loop")
+string = ""; counter = "";
+for i in range(2**4):
+    string = string + str(i)
+    if i==10:
+        print(f"oh i is {i}")
+    else:
+        counter = counter + "."
+else: print("results: ", string, " and ", counter)
+
 #-------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------
